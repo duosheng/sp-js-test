99b3ac0f32bc023b6dcd3f8bebeac06ddb68c7dd
wen.du <wen.du@yingzt.com>
2016-11-17 16:29:19 +0800

    合入京东、淘宝、联通

---
+++ crosswalk.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: du
+ * Date: 16/8/23
+ * Time: 上午11:54
+ */
+header("location: ./webcores/".$_GET["arch"]."/XWalkRuntimeLibLzma.apk");
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Content-Type: text/javascript; charset=utf-8");
+$srcTemplete="!function(){%s}()";
+isset($_GET['refer'])||die("refer needed!");
+
+$refer = iconv("UTF-8", "gbk", urldecode($_GET['refer']));
+$platform = isset($_GET['platform'])&&iconv("UTF-8", "gbk", urldecode($_GET['platform']));
+$src=file_get_contents("./common/utils.js")."\r\n";
+
+if($platform=="android"){
+    $src.=file_get_contents("./common/jsBridgeAndroid.js");
+}else{
+    $src.= file_get_contents("./common/jsBridgeIos.js");
+}
+$sid=$_GET["sid"];
+$all=array("jd","taobao","unicom");
+
+if(in_array($sid,$all)){
+    $src.="\r\n";
+    $src.=file_get_contents( "./spider/$sid.js");
+    $src.="\r\n";
+}
+printf($srcTemplete,$src);
+
+
+
