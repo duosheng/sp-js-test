d6f5fe28e3561f28daf54599a115e01d4b988c9a
wen.du <wen.du@yingzt.com>
2016-11-23 17:34:34 +0800

    邮箱脚本修改

--- emails/email.js
+++ emails/email.js
@@ -38,14 +38,6 @@ dSpider("email",function(session,env,$) {
             log("set wd start")
             dSpiderLocal.set("wd", wd.val())
             btn.text("加载中...").attr("disabled", "disabled");
-            //if (suffix == "126.com" && window._xy/*&&_xy.getExtraData().webcore=="sys"*/) {
-            //    _xy.openWithSpecifiedCore(url[suffix], "cs");
-            //}
-            if (suffix == "qq.com" && window._xy/*&&_xy.getExtraData().webcore=="sys"*/) {
-                _xy.openWithSpecifiedCore(url[suffix], "sys");
-                return;
-            }
-            //location.href=url[suffix];
             session.load(url[suffix],{Referer:""})
         } else {
             alert("暂不支持后缀为%s的邮箱".format(url[suffix]));
