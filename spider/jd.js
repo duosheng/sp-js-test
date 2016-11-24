3b958495637655c325f769de282df979bac58090
wen.du <wen.du@yingzt.com>
2016-11-24 17:57:54 +0800

    京东脚本修改

--- spider/jd.js
+++ spider/jd.js
@@ -40,6 +40,7 @@ dSpider("jd", function(session,env,$){
         session.setProgress(20);
 
         globalInfo = session.get(infokey);
+        log("gloabalInfo:"+JSON.stringify(globalInfo));
         global_contact_info = new contact_info([]);
         var taskAddr = [];
         var urlarray = $(".ia-r");
