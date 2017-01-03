9a006aa63046a5baac46c84331cb47f4dd2aa597
lei.duan <lei.duan@yingzt.com>
2017-01-03 11:11:35 +0800

    update jd

--- spider/jd.js
+++ spider/jd.js
@@ -8,23 +8,19 @@ dSpider("jd", function(session,env,$){
     var globalInfo;
 
     sid = session.get("sid");
-    session.onNavigate=function(url){
-        if(url.indexOf("://plogin.m.jd.com/user")!=-1){
-            session.showProgress(true);
-            session.setProgressMax(100);
-            session.setProgress(0);
-            session.autoLoadImg(false);
-        }
-    }
+
 
 
     if (location.href.indexOf("://m.jd.com") != -1 ) {
+        session.showProgress(true);
+        session.setProgressMax(100);
+        session.autoLoadImg(false);
         session.setProgress(5);
 
         if($(".jd-search-form-input")[0] != undefined){
             sid  = $(".jd-search-form-input")[0].children[0].value;
             session.set("sid",  sid);
-        }
+         }
 
         session.set(infokey, new info({},{},{}));
         globalInfo = session.get(infokey);
@@ -43,27 +39,27 @@ dSpider("jd", function(session,env,$){
         var taskAddr = [];
         var urlarray = $(".ia-r");
         for(var i=0;i<urlarray.length;i++){
-            taskAddr.push($.get(urlarray[i],function(response,status){
-                var node = $("<div>").append($(response))
-                var name = node.find("#uersNameId")[0].value;
-                var phone = node.find("#mobilePhoneId")[0].value;
-                var addr = node.find("#addressLabelId")[0].innerHTML;
-                var detail = node.find("#address_where")[0].innerHTML;
+                                    taskAddr.push($.get(urlarray[i],function(response,status){
+                                    var node = $("<div>").append($(response))
+                                    var name = node.find("#uersNameId")[0].value;
+                                    var phone = node.find("#mobilePhoneId")[0].value;
+                                    var addr = node.find("#addressLabelId")[0].innerHTML;
+                                    var detail = node.find("#address_where")[0].innerHTML;
 
-                global_contact_info.contact_detail.push(new contact(name,addr,detail,phone, ""));
-            }) );
+                                    global_contact_info.contact_detail.push(new contact(name,addr,detail,phone, ""));
+                                    }) );
 
-        }
+            }
 
 
-        $.when.apply($,taskAddr).done(
-            // $.when(taskAddr).done(
-            function(){
-                globalInfo.contact_info = global_contact_info;
-                saveInfo();
-                session.setProgress(30);
-                getOrder();
-            });
+          $.when.apply($,taskAddr).done(
+       // $.when(taskAddr).done(
+                  function(){
+                        globalInfo.contact_info = global_contact_info;
+                        saveInfo();
+                        session.setProgress(30);
+                        getOrder();
+                        });
 
 
     }
@@ -76,54 +72,54 @@ dSpider("jd", function(session,env,$){
         globalInfo.order_info = new order_info([]);
         globalInfo.order_info.order_detail = [];
         function getPageOrder(page){
-            $.getJSON("http://home.m.jd.com//newAllOrders/newAllOrders.json?sid="+sid+"&page="+page,function(d){
-                page++;
-                if( globalInfo.order_info.order_detail.length <=  max_order_num && d.orderList.length!=0 && (orders.order_detail.length == 0 || d.orderList[d.orderList.length-1].orderId != orders.order_detail[orders.order_detail.length-1].orderId) ){
-                    orders.order_detail = orders.order_detail.concat(d.orderList);
-                    var task = [];
-                    var tempOrder = [];
-                    if(globalInfo.order_info.order_detail.length < max_order_num){
+           $.getJSON("http://home.m.jd.com//newAllOrders/newAllOrders.json?sid="+sid+"&page="+page,function(d){
+               page++;
+               if( globalInfo.order_info.order_detail.length <=  max_order_num && d.orderList.length!=0 && (orders.order_detail.length == 0 || d.orderList[d.orderList.length-1].orderId != orders.order_detail[orders.order_detail.length-1].orderId) ){
+                   orders.order_detail = orders.order_detail.concat(d.orderList);
+                   var task = [];
+                   var tempOrder = [];
+                   if(globalInfo.order_info.order_detail.length < max_order_num){
                         if(d.orderList.length + globalInfo.order_info.order_detail.length > max_order_num){
-                            d.orderList = d.orderList.slice(0, max_order_num -  globalInfo.order_info.order_detail.length);
+                           d.orderList = d.orderList.slice(0, max_order_num -  globalInfo.order_info.order_detail.length);
                         }
                         task.push($.each(d.orderList,function(i,e){
-                            $.get("http://home.m.jd.com/newAllOrders/queryOrderDetailInfo.action?orderId="+ d.orderList[i].orderId+"&from=newUserAllOrderList&passKey="+d.passKeyList[i]+"&sid="+sid,
-                                function(response,status){
-                                    var addr = $("<div>").append($(response)).find(".step2-in-con").text();
-                                    var orderitem = new order(d.orderList[i].orderId,d.orderList[i].dataSubmit,d.orderList[i].price,addr);
+                                           $.get("http://home.m.jd.com/newAllOrders/queryOrderDetailInfo.action?orderId="+ d.orderList[i].orderId+"&from=newUserAllOrderList&passKey="+d.passKeyList[i]+"&sid="+sid,
+                                                                                             function(response,status){
+                                                                                                   var addr = $("<div>").append($(response)).find(".step2-in-con").text();
+                                                                                                   var orderitem = new order(d.orderList[i].orderId,d.orderList[i].dataSubmit,d.orderList[i].price,addr);
 
-                                    orderitem.products = [];
-                                    var products = $("<div>").append($(response)).find(".pdiv");
-                                    $.each(products,function(k, e){
-                                        var name = $("<div>").append(products[k]).find(".sitem-m-txt").text();
-                                        var price = $("<div>").append(products[k]).find(".sitem-r").text();
-                                        var num = $("<div>").append(products[k]).find(".s3-num").text();
-                                        orderitem.products.push(new product(name,  num ,price));
-                                    });
-                                    if(Date.parse(new Date()) < ((new Date(orderitem.time.split(" ")[0])).getTime() + max_order_date * 24 * 60 * 60 * 1000)){
-                                        if(globalInfo.order_info.order_detail.length < max_order_num){
-                                            globalInfo.order_info.order_detail.push(orderitem);
-                                        }
-                                    }
-                                });
-                        }));
-                    }
+                                                                                                   orderitem.products = [];
+                                                                                                   var products = $("<div>").append($(response)).find(".pdiv");
+                                                                                                   $.each(products,function(k, e){
+                                                                                                       var name = $("<div>").append(products[k]).find(".sitem-m-txt").text();
+                                                                                                       var price = $("<div>").append(products[k]).find(".sitem-r").text();
+                                                                                                       var num = $("<div>").append(products[k]).find(".s3-num").text();
+                                                                                                       orderitem.products.push(new product(name,  num ,price));
+                                                                                                   });
+                                                                                                   if(Date.parse(new Date()) < ((new Date(orderitem.time.split(" ")[0])).getTime() + max_order_date * 24 * 60 * 60 * 1000)){
+                                                                                                           if(globalInfo.order_info.order_detail.length < max_order_num){
+                                                                                                                 globalInfo.order_info.order_detail.push(orderitem);
+                                                                                                           }
+                                                                                                   }
+                                                                                             });
+                                         }));
+                   }
 
 
-                    $.when(task).done(function(){
-                        getPageOrder(page);
-                        globalInfo.order_info.order_detail.sort(compare());
-                    });
+                      $.when(task).done(function(){
+                           getPageOrder(page);
+                           globalInfo.order_info.order_detail.sort(compare());
+                      });
 
-                }else {
-                    saveInfo();
-                    session.setProgress(60);
-                    getUserInfo();
-                    return;
-                }
-            });
-        }
-        getPageOrder(1);
+               }else {
+                  saveInfo();
+                  session.setProgress(60);
+                  getUserInfo();
+                  return;
+               }
+           });
+       }
+       getPageOrder(1);
     }
 
     function compare(){
@@ -131,16 +127,16 @@ dSpider("jd", function(session,env,$){
             var value1 = (new Date(a.time.split(" ")[0])).getTime();
             var value2 = (new Date(b.time.split(" ")[0])).getTime();
             return value2 - value1;
-        }
+    }
     }
 
     function getUserInfo(){
-        location.href = "http://home.m.jd.com/user/accountCenter.action";
+           location.href = "http://home.m.jd.com/user/accountCenter.action";
     }
     if (location.href.indexOf("home.m.jd.com/user/accountCenter.action") != -1) {
         session.setProgress(70);
         if($('#shimingrenzheng')[0] != undefined){
-            $('#shimingrenzheng')[0].click();
+           $('#shimingrenzheng')[0].click();
         }
     }
 
@@ -200,9 +196,9 @@ dSpider("jd", function(session,env,$){
 
     function info(base_info,contact_info,order_info ){
         this.site_id = 2;
-        this.base_info = base_info;
-        this.contact_info = contact_info;
-        this.order_info  = order_info;
+    　　 this.base_info = base_info;
+    　　 this.contact_info = contact_info;
+    　　 this.order_info  = order_info;
     }
 
     function base_info(username, name, idcard_no, phone){
